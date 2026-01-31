a35991ba93c80cfde1c0d56be61e51b54ae9705e
discovergcs-est <nathanaolle2@gmail.com>
2026-01-31 16:18:09 -0400

    Update stellar.js

--- stellar.js
+++ stellar.js
@@ -53,5 +53,28 @@ const videoCards = document.querySelectorAll('.video-card');
     window.addEventListener('scroll', toggleUpBtn);
     upBtn.addEventListener('click', scrollToTop);
 
+//NAV BAR
+ const sections = document.querySelectorAll("section");
+  const navLinks = document.querySelectorAll(".navbar a");
 
+  window.addEventListener("scroll", () => {
+    let current = "";
+
+    sections.forEach(section => {
+      const sectionTop = section.offsetTop - 180;
+      const sectionHeight = section.offsetHeight;
+
+      if (pageYOffset >= sectionTop &&
+          pageYOffset < sectionTop + sectionHeight) {
+        current = section.getAttribute("id");
+      }
+    });
+
+    navLinks.forEach(link => {
+      link.classList.remove("active");
+      if (link.getAttribute("href") === "#" + current) {
+        link.classList.add("active");
+      }
+    });
+  });
 
